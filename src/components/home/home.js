bd8b347f2b71a310f6bbb5d660b76912b42a61da
Ryan <ryan@burgettweb.net>
2016-01-29 14:18:59 -0500

    Created adder module

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,15 +6,15 @@ const home = (props = {}) => {
             <h4>Want to add two numbers together?</h4>
             <form class="form-inline">
                 <div class="form-group">
-                    <input id="js-numInput1" class="form-control input-lg text-center" type="number" value="${props.num1 || ''}" required></input>
+                    <input id="js-numInput1" class="form-control input-lg text-center" type="number" value="${props.num1}" required></input>
                 </div>
                 <div class="form-group">
                     <label>+</label>
-                    <input id="js-numInput2" class="form-control input-lg text-center" type="number" value="${props.num2 || ''}" required></input>
+                    <input id="js-numInput2" class="form-control input-lg text-center" type="number" value="${props.num2}" required></input>
                 </div>
                 <div class="form-group">
                     <label>=</label>
-                    <input id="js-sumBox" class="form-control input-lg text-center" type="text" value="${props.sum || ''}" required readonly></input>
+                    <input id="js-sumBox" class="form-control input-lg text-center" type="text" value="${props.sum}" required readonly></input>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg">Show me the sum!</button>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import layout from './components/layout';
 import home from './components/home/home';
 import about from './components/about/about';
 
+import adder from './util/adder';
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -11,12 +13,17 @@ app.get('/', (req, res) => {
     const num1 = req.query.num1 || '';
     const num2 = req.query.num2 || '';
 
+    const sum = (num1 && num2) ? adder(parseInt(num1), parseInt(num2)): '';
+
+    console.log(`sum of ${num1} and ${num2} is ${sum}`);
+
     res.send(layout({
         title: 'First Time Node Server - Home',
         activePage: 'home',
         content: home({
             num1: num1,
-            num2: num2
+            num2: num2,
+            sum: (sum || sum === 0) ? sum : ''
         })
     }));
 }).get('/about', (req, res) => {
