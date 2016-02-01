f46be97aa7d50caf8ca89458dbbdb405c5e59e8f
Ryan <ryan@burgettweb.net>
2016-02-01 02:14:43 -0500

    Added comments

--- src/main.js
+++ src/main.js
@@ -1,40 +1,43 @@
-import express from 'express';
+import express from 'express';  // import the express library
+
+import adder from './util/adder';  // import the adder module
 
 import layout from './components/layout';
 import home from './components/home/home';
 import about from './components/about/about';
 
-import adder from './util/adder';
+const app = express();  // create the server
 
-const app = express();
+app.get('/', (req, res) => {  // handle a GET request to '/'
 
-app.get('/', (req, res) => {
+    const num1 = req.query.num1 || '';  // if a num1 query param, pass into num1, else ''
+    const num2 = req.query.num2 || '';  // if a num2 query param, pass into num2, else ''
 
-    const num1 = req.query.num1 || '';
-    const num2 = req.query.num2 || '';
-
+    // if a num1 and num2 value are present,
     const sum = (num1 && num2) ? adder(parseInt(num1), parseInt(num2)): '';
 
-    res.send(layout({
+    res.send(layout({  // call our layout view function, passing in the page's title
         title: 'First Time Node Server - Home',
-        activePage: 'home',
+        activePage: 'home',  // activePage is 'home'
         content: home({
             num1: num1,
             num2: num2,
             sum: (sum || sum === 0) ? sum : ''
         })
     }));
-}).get('/about', (req, res) => {
-    res.send(layout({
+
+}).get('/about', (req, res) => {  // handle a GET request to '/about'
+
+    res.send(layout({  // call our layout view function, passing in the page's title
         title: 'First Time Node Server - About',
-        activePage: 'about',
+        activePage: 'about',  // activePage is 'about'
         content: about()
     }));
-}).use(express.static('public'));
+
+}).use(express.static('public'));  // tell our server to expose the public folder
 
 const port = 3000;
 
-const server = app.listen(port, () => {
-    'use strict';
+const server = app.listen(port, () => {  // start the server
     console.log(`App listening at port ${server.address().port}`);
 });
